0a98251b4d09ee29ac2736c76960805dd5b9163a
WagnerLBerto <wagnerlberto@hotmail.com>
2019-12-01 22:48:17 -0300

    Configurações do webpack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname,'src','index.js'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module:{
+    rules: [ ]
+  }
+};
+
